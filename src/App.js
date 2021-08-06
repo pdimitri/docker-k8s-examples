fb77103f20081c5f7801cd52abb3f85bd2258408
peter dimitri <peter.dimitri@inovex.de>
2021-08-06 09:46:56 +0200

    Homeoffice

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and laugh.
+          Edit <code>src/App.js</code> and save to reload and cheer
         </p>
         <a
           className="App-link"
